beb487db8271dfb2e741fcb8838ec62d84d473f6
Arachnid <2982287566@qq.com>
2026-05-27 16:10:35 +0800

    change to ds api

--- llm_client.py
+++ llm_client.py
@@ -6,7 +6,8 @@ from openai import OpenAI
 load_dotenv()
 
 client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+        base_url="https://platform.deepseek.com/"
 )
 
 def summarize_note(text : str) -> dict:
@@ -23,7 +24,7 @@ def summarize_note(text : str) -> dict:
     课程笔记：
     {text}
     """
-
+    
     response = client.chat.completions.create(
         model="deepseek-chat",
         messages=[
